b15b22573e12c2e911cc0f26281582d90b4e2060
Scott Breitbach <sbreitbach@my365.bellevue.edu>
2020-09-12 16:13:47 -0500

    Exploratory Data Analysis

---
+++ Week2/assignment_2.1_ScottBreitbach.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 15:36:17 2020
+
+@author: Scott Breitbach
+Class: DSC530
+Assignment: 2.1
+"""
+
+# Display the text “Hello World! I wonder why that is always the default 
+# coding text to start with”
+print("Hello World! "
+      "I wonder why that is always the default coding text to start with.")
+
+# Add two numbers together
+print("20 + 22 is", 20 + 22)
+
+# Subtract a number from another number
+print("60 - 18 is", 60 - 18)
+
+# Multiply two numbers
+print("14 x 3 is", 14 * 3)
+
+# Divide between two numbers
+print("84", chr(247),"2 is", int(84 / 2))
+
+# Concatenate two strings together (any words)
+x = "The secret to life, the universe, and everything "
+y = "is forty-two."
+z = x + y
+print(z)
+
+# Create a list of 4 items (can be strings, numbers, both)
+listFourItems = ["Item 1", "Item 2", 3, "and 4"]
+print("Here's a list of 4 items:", listFourItems)
+
+# Append an item to your list (again, can be a string, number)
+listFourItems.append("where'd this 5th item come from?")
+print("Updated list:", listFourItems)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+tupleFourItems = ("enchilada", "um", "no", "stop")
+print("Don't look now, here comes a tuple:", tupleFourItems)
+
